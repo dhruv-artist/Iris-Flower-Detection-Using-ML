c885e188148d028bdc0f4cfa60e5ad73f66462ea
Dhruba Nandi <dhrubanandi55@gmail.com>
2025-08-04 09:12:37 +0530

    Add files via upload

---
+++ Final Year Project(Iris Flower)/Iris-Flower-Prediction-Machine-Learning-Web-App-main/iris_ml.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+
+dataset = pd.read_csv('iris.data')
+
+
+X = np.array(dataset.iloc[:, 0:4])
+y = np.array(dataset.iloc[:, 4:])
+
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+
+from sklearn.svm import SVC
+classifier = SVC(kernel='linear').fit(X_train, y_train)
+
+
+pickle.dump(classifier, open('iris.pkl', 'wb'))
